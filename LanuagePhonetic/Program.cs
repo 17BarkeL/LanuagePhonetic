e1ad9494df57cfd40023f48974b409edf3d85f2d
Luke Barkess <luke.barkess@icloud.com>
2022-09-30 14:56:36 +0100

    Add project files.

---
+++ LanuagePhonetic/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanuagePhonetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] letters = new string[,]
+            {
+                {"a", "ah" },
+                {"b", "bay" },
+                {"c", "say" },
+                {"d", "day" },
+                {"e", "euh" },
+                {"f", "eff" },
+                {"g", "zhay" },
+                {"h", "ash" },
+                {"i", "ee" },
+                {"j", "zhee" },
+                {"k", "kah" },
+                {"l", "ell" },
+                {"m", "em" },
+                {"n", "en" },
+                {"o", "oh" },
+                {"p", "pay" },
+                {"q", "coo" },
+                {"r", "err" },
+                {"s", "ess" },
+                {"t", "tay" },
+                {"u", "u" },
+                {"v", "vay" },
+                {"w", "doob-leh-vay" },
+                {"x", "eeks" },
+                {"y", "ee-grek" },
+                {"z", "zed" },
+            };
+
+            Console.WriteLine("-----------------------\nFrench phonetic speller\n-----------------------");
+            
+            string word = "";
+            string phoneticSpelling = "";
+
+            while (word != "q")
+            {
+                Console.Write("Enter the word(q to quit): ");
+                word = Console.ReadLine().ToLower();
+                phoneticSpelling = "";
+
+                foreach (char character in word)
+                {
+                    for (int i = 0; i < letters.GetLength(0); i++)
+                    {
+                        if (Char.Parse(letters[i, 0]) == character)
+                        {
+                            phoneticSpelling += letters[i, 1];
+
+                            if (character != word.Last())
+                            {
+                                phoneticSpelling += "-";
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine(phoneticSpelling + "\n");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
